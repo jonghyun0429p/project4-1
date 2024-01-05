b0cab7830b9c35d35fd506148904908df807e084
jonghyun0429p <bwhdgus0429@naver.com>
2024-01-05 11:35:31 +0900

    0105

--- hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
+++ hello-spring/src/main/java/hello/hellospring/HelloSpringApplication.java
@@ -1,14 +1,13 @@
-package com.example.demo;
+package hello.hellospring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class HelloSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(HelloSpringApplication.class, args);
 	}
 
-	
 }
